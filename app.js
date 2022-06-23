3528abfa88859c11c315b8ba0e8cacd39a2d76f4
Denis <payar3282@gmail.com>
2022-06-23 14:01:11 +0300

    commiting 1st test example

---
+++ app.js
@@ -0,0 +1,52 @@
+const possibleToChooseBtns = document.querySelectorAll('.game__img')
+const playBtn = document.querySelector('.play-button')
+const gameResult = document.querySelector('.game__result')
+const retryBtn = document.querySelector('.retry-button')
+const choose = document.querySelector('.game__title')
+
+let playerSelection = null
+
+const computerPlay = () => {
+  const possibleToChoose = ['Rock', 'Paper', 'Scissors']
+  let index = possibleToChoose.length
+  return possibleToChoose[Math.floor(Math.random() * index)]
+}
+let computerSelection = computerPlay()
+
+
+possibleToChooseBtns.forEach(img => {
+  img.addEventListener('click', () => {
+    playerSelection = img.id
+    choose.innerHTML = playerSelection
+  })
+})
+
+// RULES
+const gameRules = {
+  Rock: 'Rock > Scissors',
+  Scissors: 'Scissors > Paper',
+  Paper: 'Paper > Rock'
+}
+
+// PLAYING
+const playRound = (playerSelection, computerSelection) => {
+  if (playerSelection 
+    && gameResult.innerHTML.length === 0
+    ) {
+    let win = false
+    gameRules[playerSelection] === `${playerSelection} > ${computerSelection}` ? win = true : playerSelection === computerSelection ? win = null : win = false 
+    gameResult.innerHTML = win ? `You Win! ${playerSelection} beats ${computerSelection}` : win === null ? `Ничья` : `You Lose! ${computerSelection} beats ${playerSelection}`
+    retryBtn.style.display = 'block'
+  }
+}
+
+playBtn.addEventListener('click', () => {
+  playRound(playerSelection, computerSelection)
+  } 
+)
+
+retryBtn.addEventListener('click', () => {
+  choose.innerHTML = 'CLICK TO CHOOSE'
+  gameResult.innerHTML = ''
+  computerSelection = computerPlay()
+})
